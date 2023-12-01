d25c9819dc374b190ee6de095c81547aea0138cb
CookedBear <21371068@buaa.edu.cn>
2023-12-01 21:18:07 +0800

    Portal 页面初始化

--- frontend/src/store/modules/app/index.ts
+++ frontend/src/store/modules/app/index.ts
@@ -13,11 +13,13 @@ export const useAppStore = defineStore(
         classificationReNewed: false,
         classification1: [],
         classification2: [],
+        classificationTree: [],
     }),
     getters: {},
     actions: {
         async reNewClass() {
             if (this.classificationReNewed !== true) {
+                this.classificationReNewed = true;
                 const result1 = await viewC1Api();
                 const result2 = await viewC2Api();
                 if (result1.code === 0) {
@@ -28,17 +30,40 @@ export const useAppStore = defineStore(
                             // @ts-ignore
                             state.classification1.push(res1[i].name);
                         }
-                        state.classificationReNewed = true;
+
                         console.log('Storage state ==> ', this)
                     })
+                } else {
+                    this.classificationReNewed = false;
                 }
                 if (result2.code === 0) {
-                    const res2 = result1.data;
+                    const res2 = result2.data;
                     console.log(res2);
+
                     this.$patch((state) => {
-                        for (var i = 0; i < res2.length; i++) {
+                        for (var j = 0; j < state.classification1.length; j++) {
                             // @ts-ignore
-                            state.classification2.push(res2[i].name);
+                            state.classificationTree[state.classification1[j]] = [];
+                            // @ts-ignore
+                            state.classificationTree.push({
+                                label: state.classification1[j],
+                                key: j + "-0",
+                                children: []
+                            });
+                        }
+                        for (var k = 0; k < res2.length; k++) {
+                            // @ts-ignore
+                            state.classification2.push(res2[k].name);
+                            for (var l = 0; l < state.classificationTree.length; l++) {
+                                if (state.classificationTree[l]['label'] === res2[k].classification_1_name) {
+                                    // @ts-ignore
+                                    state.classificationTree[l]['children'].push({
+                                        label: res2[k].name,
+                                        // @ts-ignore
+                                        key: state.classificationTree[l]['key'] + '-' + state.classificationTree[l]['children'].length
+                                    });
+                                }
+                            }
                         }
                         console.log('Storage state ==> ', this)
                     })
@@ -46,6 +71,29 @@ export const useAppStore = defineStore(
             }
             return this.classification1;
         },
+
+        async getC1() {
+            await this.reNewClass();
+            return this.classification1;
+        },
+
+        async getC2() {
+            if (this.classificationReNewed === true) {
+                return this.classification2;
+            } else {
+                await this.reNewClass();
+                return this.classification2;
+            }
+        },
+
+        async getCTree() {
+            if (this.classificationReNewed === true) {
+                return this.classificationTree;
+            } else {
+                await this.reNewClass();
+                return this.classificationTree;
+            }
+        }
 
     },
     persist: {
