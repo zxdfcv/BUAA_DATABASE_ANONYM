cd7336f1262986ef257cde4ccb065f0ce75737c5
zyx21371055 <21371055@buaa.edu.cn>
2023-12-05 17:23:45 +0800

    add change

--- frontend/src/views/admin/components/barChartComponent.vue
+++ frontend/src/views/admin/components/barChartComponent.vue
@@ -123,6 +123,16 @@ const initBarChart = () => {
         }
       }
     },
+    toolbox: {
+      show: true,
+      feature: {
+        mark: {show: true},
+        dataView: {show: true, readOnly: false},
+        magicType: {show: true, type: ['line', 'bar']},
+        restore: {show: true},
+        saveAsImage: {show: true}
+      }
+    },
     series: [
       {
         data: yData,
