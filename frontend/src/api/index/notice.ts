390b366d7210b37e81228f734d41f56d3537cf5f
CookedBear <21371068@buaa.edu.cn>
2023-12-22 17:48:27 +0800

    feat: 聊天室框架

--- frontend/src/api/index/notice.ts
+++ frontend/src/api/index/notice.ts
@@ -8,6 +8,10 @@ enum URL {
     readComment = '/myapp/comment/read',
     readReply = '/myapp/reply/read',
     readMention = '/myapp/mention/read',
+
+    chatList = '/myapp/chat/notice',
+    chatContent = '/myapp/chat/list',
+    chatCreate = '/myapp/chat/create',
 }
 
 const getCommentMessageApi = async (params: any) => get<any>({url: URL.commentMessage, params: params, data: {}, headers: {}});
@@ -18,8 +22,14 @@ const readCommentMessageApi = async (params: any) => post<any>({url: URL.readCom
 const readReplyMessageApi = async (params: any) => post<any>({url: URL.readReply, params: params, data: {}, headers: {}});
 const readMentionMessageApi = async (params: any) => post<any>({url: URL.readMention, params: params, data: {}, headers: {}});
 
+const getChatListApi = async (params: any) => get<any>({url: URL.chatList, params: params, data: {}, headers: {}});
+const getChatDetailApi = async (params: any) => get<any>({url: URL.chatContent, params: params, data: {}, headers: {}});
+const sendChatMessageApi = async (data: any) => post<any>({url: URL.chatCreate, params: {}, data: data, headers: {}});
+
+
 
 export {
     getCommentMessageApi, getReplyMessageApi, getMentionMessageApi,
     readCommentMessageApi, readReplyMessageApi, readMentionMessageApi,
+    getChatListApi, getChatDetailApi, sendChatMessageApi,
 };
